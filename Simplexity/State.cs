a1afe7e61594dd5f7dec1e8f20b54af9d273927c
André Santos <afbsantos1@gmail.com>
2018-04-18 21:13:58 +0100

    correction on State.cs

--- Simplexity/State.cs
+++ Simplexity/State.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Simplexity
+﻿namespace Simplexity
 {
         //Enumeração que revela o estado do tabuleiro
         public enum State { Undecided, R, r, W, w };
