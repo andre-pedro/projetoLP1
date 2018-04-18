9f402dac44766493c7bb2b916c9080be8a02fd0d
André Santos <afbsantos1@gmail.com>
2018-04-18 21:46:21 +0100

    create layout.cs and add constructor

--- Simplexity/Board.cs
+++ Simplexity/Board.cs
@@ -9,5 +9,17 @@ namespace Simplexity
     class Board
     {
         //Esta classe irá conter um array de 7x7, onde serão inseridos os elementos do jogo
+        private State[,] state;
+        public State NextTurn { get; private set; }
+
+
+        // cria o tamanho do tabuleiro
+        public Board()
+        {
+            state = new State[7, 7];
+
+            //pede ao jogador 1 para jogar cubo ou cilindro da cor branca
+            NextTurn = State.W | State.w;
+        }
     }
 }
