0f3de8cee332b6b046bb4f5c48998c8d95db0d79
hardcorelif3 <33433474+hardcorelif3@users.noreply.github.com>
2018-04-18 21:08:08 +0100

    Added Player and WinChecker classes

---
+++ Simplexity/WinChecker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplexity
+{
+    class WinChecker
+    {
+        //Verifica se algum jogador venceu
+    }
+}
