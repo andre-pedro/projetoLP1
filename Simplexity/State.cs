33af28e1f077a776e06a63d0f2f5e18730ae9d7a
André Santos <afbsantos1@gmail.com>
2018-04-18 20:46:54 +0100

    add state enum

---
+++ Simplexity/State.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplexity
+{
+        //Enumeração que revela o estado do tabuleiro
+        public enum State { Undecide, R, r, W, w };
+
+}
