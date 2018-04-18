7534a9ed83794a86a2e4a4160c9445baf600f94c
hardcorelif3 <33433474+hardcorelif3@users.noreply.github.com>
2018-04-18 21:00:46 +0100

    Added Board and Position classes

---
+++ Simplexity/Board.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplexity
+{
+    class Board
+    {
+        //Esta classe irá conter um array de 7x7, onde serão inseridos os elementos do jogo
+    }
+}
