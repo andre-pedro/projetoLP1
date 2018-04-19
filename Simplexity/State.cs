f1e941ab098aacd05cdcd4ce912ffefed906a4bf
André Santos <afbsantos1@gmail.com>
2018-04-19 23:47:30 +0100

    add doxy comments on enum

--- Simplexity/State.cs
+++ Simplexity/State.cs
@@ -1,6 +1,14 @@
 ﻿namespace Simplexity
 {
-    //Enumeração que revela o estado do tabuleiro
+    /// <summary>
+    /// Enumeração para as possiveis peças no jogo
+    /// Undecided - o espaço é preenchido com |
+    /// R - Cubos brancos 
+    /// r - Cilindros brancos 
+    /// W - Cubos brancos 
+    /// w - Cilindros brancos
+    /// </summary>
+
     public enum State { Undecided, R, r, W, w };
 
 }
