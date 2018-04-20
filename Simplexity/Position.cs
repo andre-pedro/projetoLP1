6e8efe336ccb9afc89360c97e4cc83eecad54824
André Santos <afbsantos1@gmail.com>
2018-04-21 00:17:59 +0100

    add doxy comment to Position.cs

--- Simplexity/Position.cs
+++ Simplexity/Position.cs
@@ -6,23 +6,28 @@ using System.Threading.Tasks;
 
 namespace Simplexity
 {
-
+    /// <summary>
+    /// Esta classe irá representar a localização
+    /// da linha e coluna no tabuleiro
+    /// </summary>
     class Position
     {
-
-        /// <summary>
-        /// Esta classe irá representar a localização
-        /// da linha e coluna no tabuleiro
-        /// </summary>
 
         public int Linha { get; }
         public int Coluna { get; }
 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="linha"></param>
+        /// <param name="coluna"></param>
         public Position(int linha, int coluna)
         {
             Linha = linha;
             Coluna = coluna;
         }
+
 
         public Position(int coluna)
         {
