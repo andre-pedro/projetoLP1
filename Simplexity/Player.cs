df6cdfb017460f1cd3af0ef979047523427bc71f
hardcorelif3 <33433474+hardcorelif3@users.noreply.github.com>
2018-04-20 18:45:11 +0100

    Added switch to Player class

--- Simplexity/Player.cs
+++ Simplexity/Player.cs
@@ -10,11 +10,36 @@ namespace Simplexity
     {
         //Esta classe irá obter o input inserido pelo jogador
         public Position ColumnPosition(Board board)
-        Console.WriteLine("Escolha a coluna onde quer jogar");    
-        int readpositionnumber = Convert.ToInt32(Console.ReadLine());
-        return readpositionnumber;
+        {
+            Console.WriteLine("Escolha a coluna onde quer jogar");
+            int colInput = Convert.ToInt32(Console.ReadLine());
+            return colInput;
+        }
 
-
+        private Position NumberPosition(int colInput)
+        {
+            switch (colInput)
+            {
+                case 1:
+                    return new Position(1);
+                case 2:
+                    return new Position(2);
+                case 3:
+                    return new Position(3);
+                case 4:
+                    return new Position(4);
+                case 5:
+                    return new Position(5);
+                case 6:
+                    return new Position(6);
+                case 7:
+                    return new Position(7);
+                default:
+                    return ;
+            }
+        }
     }
+
+}
 
 }
