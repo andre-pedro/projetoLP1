7add7087212b568be774c626784d944252109d79
Tiago Alves <tfaon@sapo.pt>
2018-04-20 21:30:27 +0100

    Add simulated board

--- Simplexity/Program.cs
+++ Simplexity/Program.cs
@@ -32,10 +32,10 @@ namespace Simplexity
             string s4 = "Prima [ENTER] para começar...";
 
             // Parametros
-            private int cubosBrancos;
-            private int cilindrosBrancos;
-            private int cubosVermelhos;
-            private int cilindrosVermelhos;
+            int cubosBrancos;
+            int cilindrosBrancos;
+            int cubosVermelhos;
+            int cilindrosVermelhos;
 
             // Start
             Console.ForegroundColor = ConsoleColor.Gray;
@@ -50,7 +50,48 @@ namespace Simplexity
             Console.WriteLine();
             Console.WriteLine(s4.PadLeft(s4.Length + 0));
             Console.ReadKey();
+            // Turnos
+            for (int turno = 0; turno <= 1000; turno++)
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Clear();
+                Console.WriteLine("[Show_World_Test] ");
+                Console.WriteLine();
+                Console.WriteLine("7      |   |   |   |   |   |   |");
+                Console.WriteLine("6      |   |   |   |   |   |   |");
+                Console.WriteLine("5      |   |   |   |   |   |   |");
+                Console.WriteLine("4      |   |   |   |   |   |   |");
+                Console.WriteLine("3      |   |   |   |   |   |   |");
+                Console.WriteLine("2      |   |   |   |   |   |   |");
+                Console.WriteLine("1      |   |   |   |   |   |   |");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("       1   2   3   4   5   6   7");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.Write("[Jogador 1 - ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Branco");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("]");
+                Console.WriteLine();
+                Console.WriteLine("Cubos Brancos: " + cubosBrancos);
+                Console.WriteLine("Cilindros Brancos: " + cilindrosBrancos);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.Write("[Jogador 2 - ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Vermelho");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("]");
+                Console.WriteLine();
+                Console.WriteLine("Cubos Vermelhos: " + cubosVermelhos);
+                Console.WriteLine("Cilindros Vermelhos: " + cilindrosVermelhos);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("[Turno: " + turno + "]");
+                Console.WriteLine();
+            }
         }
-
     }
 }
