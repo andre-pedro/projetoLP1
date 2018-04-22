9bcad9151a48c9a2a016fee033ed812a40652bd7
André Santos <afbsantos1@gmail.com>
2018-04-22 19:01:22 +0100

    working on WinChecker.cs

--- Simplexity/WinChecker.cs
+++ Simplexity/WinChecker.cs
@@ -20,17 +20,129 @@ namespace Simplexity
             int count;
             int diagonal;
 
-            for (int i=0; i<array.GetLength(0);i++)
+
+            //verifica no array bidimensional
+            for (int i = 0; i < array.GetLength(0); i++)
             {
                 if (teste == true) break;
-                for(int j=0; j<array.GetLength(1);j++)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
                     if (teste == true)
-                        break;  
+                        break;
+
+                    count = 0;
+                    //Horizontal verification 
+                    /* k is X coord which cycles from actual position to end of row */
+                    for (int k = i; k < array.GetLength(0); k++)
+                    {
+                        /* If number at [X,Y] is equal to n, increment counter */
+                        if (array[k, j] == n)
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            count = 0;
+                        }
+                        /* If counter is >= 4, four in line is found */
+                        if (count >= 4)
+                        {
+                            teste = true;
+                            break;
+                        }
+                    }
+
+                    /* Vertical verification */
+                    /* k is Y coord which cycles from actual position to end of row */
+                    for (int k = j; k < array.GetLength(1); k++)
+                    {
+                        /* If number at [X,Y] is equal to n, increment counter */
+                        if (array[i, k] == n)
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            count = 0;
+                        }
+                        /* If counter is >= 4, four in line is found */
+                        if (count >= 4)
+                        {
+                            teste = true;
+                            break;
+                        }
+                    }
+
+                    /* Counter is reset before every verification */
+                    count = 0;
                 }
+                diagonal = i; /* diagcounter receives actual X */
+                              /* k is Y coord which cycles from actual position to end of row */
+                for (int k = j; k < array.GetLength(1); k++)
+                {
+                    /* When diagcounter (X) value is offgrid, break cycle */
+                    if (diagonal == array.GetLength(0))
+                    {
+                        break;
+                    }
+
+                    /* If number at [X,Y] is equal to n, increment counter */
+                    if (array[diagonal, k] == n)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+
+                    /* If counter is >= 4, four in line is found */
+                    if (count >= 4)
+                    {
+                        teste = true;
+                        break;
+                    }
+                    /* diagcounter (X coord moves 1 to the right) */
+                    diagonal++;
+                }
+                /* Counter is reset before every verification */
                 count = 0;
+
+                /* From right to left diagonal (upside down) */
+                diagonal = i; /* diagcounter receives actual X */
+                              /* k is Y coord which cycles from actual position to end of row */
+                for (int k = j; k < array.GetLength(1); k++)
+                {
+                    /* When diagcounter (X) value is offgrid, break cycle */
+                    if (diagonal < 0)
+                    {
+                        break;
+                    }
+
+                    /* If number at [X,Y] is equal to n, increment counter */
+                    if (array[diagonal, k] == n)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+
+                    /* If counter is >= 4, four in line is found */
+                    if (count >= 4)
+                    {
+                        teste = true;
+                        break;
+                    }
+                    /* diagcounter (X coord moves 1 to the left) */
+                    diagonal--;
+                }
             }
-            return true;
+            return teste;
         }
+
+
     }
 }
+
